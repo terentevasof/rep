7c9a25b3564428fc2668a182aa7038e7fee22a09
Alexey <alexey92177@gmail.com>
2023-02-27 17:12:13 +0300

    task 1

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -96,7 +96,7 @@ IncludeTemplateLangFile(__FILE__);
 
               <ul class="site-menu js-clone-nav d-none d-lg-block">
                 <li class="active">
-                  <a href="index.html">Home</a>
+                  <a href="index.html">Home1</a>
                 </li>
                 <li class="has-children">
                   <a href="properties.html">Properties</a>
